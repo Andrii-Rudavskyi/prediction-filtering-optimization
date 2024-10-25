35a4f680118d0668f762b571f65e94dc3463383f
Andrii-Rudavskyi <144094558+Andrii-Rudavskyi@users.noreply.github.com>
2024-10-25 13:28:44 +0200

    Plot three singnals and error + add comments

--- analysis.py
+++ analysis.py
@@ -4,21 +4,28 @@ import numpy as np
 import os
 import seaborn as sn
 
-from helper2 import calculate_delay_error, calculate_predicted_error, predict
+from helper2 import calculate_delay_error, calculate_predicted_error, predict, PredictionModel, extract_noise_2, PredictionModelType
 
 dataFolder = './data/'
 imagesFolder = './images/'
 
-latency = 0.1
+# Set latency in seconds
+latency = 0.10
 
 # Focal distance to convert to angular space. TODO: Load it properly from intrinsics (product/device specific).
 fx = 330
 fy = 330
 
+#selection of the prediction model
+predictionModel = PredictionModel(PredictionModelType.PolynomialFit)
+if (predictionModel.modelType == PredictionModelType.PolynomialFit):
+    predictionModel.setBufferSize(6)
+    predictionModel.setPolynomialOrder(2)
+
 for filename in os.listdir(dataFolder):
     if filename.endswith('cnsdk_blink_raw_data_trace.csv'):
+        #Extract trace from file
         eyetrackerdata = pd.read_csv(dataFolder + filename)
-        imudata = pd.read_csv(dataFolder + filename)
 
         eyetrackerdata_timestapms = eyetrackerdata[' cameraTimestamp']
         #Start trace from zero
@@ -32,23 +39,29 @@ for filename in os.listdir(dataFolder):
         signal = [leftEyeX, leftEyeY, leftEye3DZ]
         signal_label = ['leftEyeX', 'leftEyeY', 'leftEye3DZ']
 
-        #filtered_time, filtered_signal, noise = extract_noise_2(signal[0], eyetrackerdata_timestapms)
-        delay_error = calculate_delay_error(eyetrackerdata_timestapms, signal[0], predictionTime=latency)
-        predicted = predict(eyetrackerdata_timestapms, signal[0], predcictionTime=latency)
-        ground_truth, predicted_error = calculate_predicted_error(eyetrackerdata_timestapms, signal[0], predicted, predictionTime=latency)
-        fig, ax1 = plt.subplots()
+        for i in range(0, 3):
+            # filtered_time, filtered_signal, noise = extract_noise_2(signal[0], eyetrackerdata_timestapms)
+            delay_error = calculate_delay_error(eyetrackerdata_timestapms, signal[i], predictionTime=latency)
 
-        ax1.plot(eyetrackerdata_timestapms, signal[0], color='blue')
-        ax1.plot(eyetrackerdata_timestapms, ground_truth, color='black')
-        ax1.plot(eyetrackerdata_timestapms, predicted, color='g')
-        ax1.legend(['Original delayed signal', 'Ground truth signal', 'Fully latency compensated signal'])
-        ax1.set_ylabel("Signal, signal units")
+            predicted = predict(eyetrackerdata_timestapms, signal[i], predictionModel, predcictionTime=1.0 * latency)
+            # predicted = predict(eyetrackerdata_timestapms, signal[0], PredictionModel.PolynomialFit, predcictionTime=latency)
+            ground_truth, predicted_error = calculate_predicted_error(eyetrackerdata_timestapms, signal[i], predicted,
+                                                                      predictionTime=latency)
 
-        ax2 = ax1.twinx()
-        ax2.plot(eyetrackerdata_timestapms, delay_error, '--', color='r', linewidth=1.0)
-        ax2.plot(eyetrackerdata_timestapms, predicted_error, marker='.', color="red", mfc='none', linewidth=0.5)
-        ax2.legend(['Delay error (without prediction)', 'Error after full latency compensation (last point velosity model)'])
-        ax2.set_ylabel("Error, signal units", color='r')
+            plt.subplot(3, 2, 2 * i + 1)
+            plt.plot(eyetrackerdata_timestapms, signal[i], color='blue')
+            plt.plot(eyetrackerdata_timestapms, ground_truth, color='black')
+            plt.plot(eyetrackerdata_timestapms, predicted, color='g')
+            plt.title('Signal:  ' + signal_label[i])
+            plt.legend(['Original delayed signal', 'Ground truth signal', 'Fully latency compensated signal'])
+            plt.ylabel("Signal, signal units")
+
+            plt.subplot(3, 2, 2 * i + 2)
+            plt.plot(eyetrackerdata_timestapms, delay_error, '--', color='b', linewidth=2.0)
+            plt.plot(eyetrackerdata_timestapms, predicted_error, marker='.', color="red", mfc='none', linewidth=0.5)
+            plt.title('Error:  ' + signal_label[i])
+            plt.legend(['Delay error (without prediction)', 'Error after full latency compensation (last point velosity model)'])
+            plt.ylabel("Error, signal units")
 
         plt.show()
 
